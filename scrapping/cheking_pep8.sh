63b80e694c3b639e262f83ff81ce74cb1d6cf7b0
felix marquez <fmarquez@fenix>
2020-04-04 20:20:51 -0400

    first commit

--- scrapping/cheking_pep8.sh
+++
@@ -1,8 +0,0 @@
-#kemok pep8 error counter
-
-if [ -f $HOME/.local/bin/pep8 ];
-then
-   $HOME/.local/bin/pep8 $HOME/.local/bin/pep8 $1  | wc -l
-else
-echo   "pep8 is not installed, need install it to python 3"
-fi
